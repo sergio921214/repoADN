a5ca55042c224d300e63bff71b9e25b7439b341c
Sergio Bermúdez Tarazona - Ceiba Software <sergio.bermudez@ceiba.com.co>
2021-01-02 13:10:57 -0500

    se agrega logica para testear calculo de dias de semana y fin semana

--- java-arquitectura-hexagonal/comun/comun-dominio/src/test/java/com/ceiba/utilidades/UtilidadesFechaTest.java
+++ java-arquitectura-hexagonal/comun/comun-dominio/src/test/java/com/ceiba/utilidades/UtilidadesFechaTest.java
@@ -1,6 +1,10 @@
 package com.ceiba.utilidades;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.ceiba.BasePrueba;
+import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -11,18 +15,61 @@ import com.ceiba.dominio.utilidades.UtilidadesFecha;
 
 public class UtilidadesFechaTest {
 
-	  @Test
-	  public void convertirStringADateTest() {
-	    // Crear calendar para test
-	    Calendar cal = Calendar.getInstance();
-	    cal.set(2020, 11, 25,0,0,0);
-	    cal.set(Calendar.MILLISECOND, 0);
-	    // Crear String de fecha
-	    String strFecha = "2020-12-25";
-	    // Obtener resultado
-	    Date fecha = UtilidadesFecha.convertirStringADate(strFecha,"yyyy-MM-dd");
-	    // Validar resultado
-	    assertEquals(cal.getTime(), fecha);
-	  }
+	@Test
+	public void convertirStringADateTest() {
+		// Crear calendar para test
+		Calendar cal = Calendar.getInstance();
+		cal.set(2020, 11, 25, 0, 0, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		// Crear String de fecha
+		String strFecha = "2020-12-25";
+		// Obtener resultado
+		Date fecha = UtilidadesFecha.convertirStringADate(strFecha, "yyyy-MM-dd");
+		// Validar resultado
+		assertEquals(cal.getTime(), fecha);
+	}
 
+	@Test
+	public void validarFormatoFechaInvalidoTest() {
+		// Crear calendar para test
+		Calendar cal = Calendar.getInstance();
+		cal.set(2020, 11, 25, 0, 0, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		// Crear String de fecha
+		String strFecha = "2020/12/25";
+		// Obtener resultado
+		BasePrueba.assertThrows(() -> UtilidadesFecha.convertirStringADate(strFecha, "yyyy-MM-dd"), ExcepcionValorInvalido.class,"Se debe ingresar un formato de fecha yyyy-mm-dd");
+	}
+	
+	public void contarDiasFinSemanaTest() {
+		// Crear calendar para test
+		Calendar cal1 = Calendar.getInstance();
+		Calendar cal2 = Calendar.getInstance();
+		cal1.set(2020, 11, 25, 0, 0, 0);
+		cal1.set(Calendar.MILLISECOND, 0);
+		cal2.set(2020, 11, 30, 0, 0, 0);
+		cal2.set(Calendar.MILLISECOND, 0);
+		
+
+		// Act
+		Integer diasFinSemana = UtilidadesFecha.contarDiasFinSemana(cal1.getTime(),cal2.getTime());
+		// Assert
+		assertTrue(diasFinSemana==2);
+	}
+
+	public void contarDiasSemanaTest() {
+		// Crear calendar para test
+		Calendar cal1 = Calendar.getInstance();
+		Calendar cal2 = Calendar.getInstance();
+		cal1.set(2020, 11, 25, 0, 0, 0);
+		cal1.set(Calendar.MILLISECOND, 0);
+		cal2.set(2020, 11, 30, 0, 0, 0);
+		cal2.set(Calendar.MILLISECOND, 0);
+		
+
+		// Act
+		Integer diasSemana = UtilidadesFecha.contarDiasDeSemana(cal1.getTime(),cal2.getTime());
+		// Assert
+		assertTrue(diasSemana==3);
+	}
 }

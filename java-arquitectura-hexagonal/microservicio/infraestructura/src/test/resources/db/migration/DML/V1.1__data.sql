aff41c8bac149a730766f50163ccf98b9dfc7e77
Sergio Bermúdez Tarazona - Ceiba Software <sergio.bermudez@ceiba.com.co>
2021-01-03 23:27:55 -0500

    se agrega schema sql para crear tablas durante test

--- java-arquitectura-hexagonal/microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
+++ java-arquitectura-hexagonal/microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
@@ -1,4 +1,3 @@
-insert into usuario(nombre,clave,fecha_creacion) values('test','1234',now());
 insert into auto(placa, tipoCombustible, rentado, precioPorDia, multiplicadorFinSemana) values('BZA485', 'GAS', 0, '50000', '3');
 insert into auto(placa, tipoCombustible, rentado, precioPorDia, multiplicadorFinSemana) values('BZA486', 'ELECTRICO', 1, '60000', '3');
 INSERT INTO `tarifa` (`tipoCombustible`, `porcentaje`) VALUES ('GAS', '1');

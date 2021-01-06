39a2f31c6033d2629f200338a23fd894fb4e4c6c
Sergio Bermúdez Tarazona - Ceiba Software <sergio.bermudez@ceiba.com.co>
2021-01-06 10:22:36 -0500

    se corrige prueba integracion para listar autos

--- java-arquitectura-hexagonal/microservicio/infraestructura/src/test/java/com/ceiba/auto/controlador/ConsultaControladorAutoTest.java
+++ java-arquitectura-hexagonal/microservicio/infraestructura/src/test/java/com/ceiba/auto/controlador/ConsultaControladorAutoTest.java
@@ -1,9 +1,12 @@
 package com.ceiba.auto.controlador;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,46 +18,46 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.ceiba.ApplicationMock;
+import com.ceiba.auto.modelo.dto.DtoAuto;
+import com.ceiba.auto.puerto.dao.DaoAuto;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes= ApplicationMock.class)
+@ContextConfiguration(classes = ApplicationMock.class)
 @WebMvcTest(ConsultaControladorAuto.class)
 public class ConsultaControladorAutoTest {
-	
-    @Autowired
-    private MockMvc mocMvc;
 
-    @Test
-    public void listar() throws Exception {
-        // arrange
+	@Autowired
+	private MockMvc mocMvc;
+	@Autowired
+	private DaoAuto daoAuto;
 
-        // act - assert
-        mocMvc.perform(get("/autos")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].placa", is("BZA485")));
-    }
-    
-    @Test
-    public void listarDisponibles() throws Exception {
-        // arrange
+	@Test
+	public void listar() throws Exception {
+		// arrange
+		List<DtoAuto> retornoAutos;
+		// act - assert
+		retornoAutos = daoAuto.listar();
+		mocMvc.perform(get("/autos").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].placa", is(retornoAutos.get(0).getPlaca())));
 
-        // act - assert
-        mocMvc.perform(get("/autos/disponibles")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())                
-                .andExpect(jsonPath("$[0].placa", is("BZA485")));
-    }
-    
-    @Test
-    public void listarRentados() throws Exception {
-        // arrange
+	}
 
-        // act - assert
-        mocMvc.perform(get("/autos/rentados")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].placa", is("BZA486")));
-    }
+	@Test
+	public void listarDisponibles() throws Exception {
+		// arrange
+
+		// act - assert
+		mocMvc.perform(get("/autos/disponibles").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].placa", is("BZA485")));
+	}
+
+	@Test
+	public void listarRentados() throws Exception {
+		// arrange
+
+		// act - assert
+		mocMvc.perform(get("/autos/rentados").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].placa", is("BZA486")));
+	}
 
 }
